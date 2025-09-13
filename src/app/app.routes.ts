703588062ba0aa6e1bffdf030858cea0218ce6ac
Rahman <samiur.rahman.378@my.csun.edu>
2025-09-12 18:18:27 -0700

    9/12/25 Update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Home } from './home/home';
+import { Weather } from './weather/weather';
+import { City } from './city/city';
+import { Country } from './country/country';
+export const routes: Routes = [
+      { path: '', component: Home, pathMatch: 'full' },
+      { path: 'weather', component: Weather },
+      { path: 'city', component: City },
+      { path: 'country', component: Country }
+];
