0f6acd2509168424889652b19015cef9ebc76ce4
dracuva <samiurrahman003@gmail.com>
2025-10-17 18:28:36 -0700

    10/17/2025

---
+++ src/app/country-population/population-data.ts
@@ -0,0 +1,6 @@
+export interface PopulationData {
+    id: string;
+    name: string;
+    iso2: string;
+    iso3: string;
+}
--- src/app/country/country.ts
+++ src/app/country/country.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { CountryData } from './country-data';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-country',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './country.html',
   styleUrl: './country.scss'
 })
 export class Country {
-
+    countries: CountryData[] = [];
+    constructor(http: HttpClient) {
+    http.get<CountryData[]>(environment.apiUrl + "api/countries").subscribe(data => {
+      this.countries = data;
+    });
+  }
 }
+
+
